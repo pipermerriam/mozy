73051a5b8e337f88d78602f5df650eefa3a38068
Piper Merriam <piper@simpleenergy.com>
2015-04-16 15:53:22 -0600

    stuff

--- mozy/apps/mosaic/backends/brute.py
+++ mozy/apps/mosaic/backends/brute.py
@@ -1,5 +1,7 @@
 import functools
 
+from django.utils.functional import SimpleLazyObject
+
 from mozy.apps.mosaic.utils import (
     cast_image_data_to_scipy_array,
 )
@@ -24,6 +26,18 @@ def load_stock_data(tile_size):
     return tuple(stock_data)
 
 
+def reset_stock_data():
+    global STOCK_DATA
+    STOCK_DATA = SimpleLazyObject(
+        lambda: load_stock_data(tile_size=tile_size)
+    )
+
+
+STOCK_DATA = SimpleLazyObject(
+    lambda: load_stock_data(tile_size=tile_size)
+)
+
+
 def measure_similarity(tile_data, stock_data):
     """
     Naive euclidean distance measure of the RGB tile data.
@@ -33,8 +47,9 @@ def measure_similarity(tile_data, stock_data):
     return measure
 
 
-def find_tile_match(tile_data, stock_data, exclusions):
-    best_match = 1e12  # sufficiently large to not be a good match
+def find_tile_match(tile_data, stock_data, exclusions, match_threshold=None):
+    # 255 * 3 colors * 20 pixels * 20 pixels
+    best_match = 255 * 3 * 20 * 20  # 306000
     best_match_id = None
 
     for data, stock_id in stock_data:
@@ -47,26 +62,37 @@ def find_tile_match(tile_data, stock_data, exclusions):
             best_match = similarity
             best_match_id = stock_id
 
+        if match_threshold and similarity <= match_threshold:
+            break
+
     exclusions.add(best_match_id)
 
     return best_match_id, best_match
 
 
-def BruteForceTileMatcher(tile_size, exclusions=None):
+def BruteForceBestTileMatcher(tile_size, exclusions=None):
     if exclusions is None:
         exclusions = set()
-    stock_data = load_stock_data(tile_size=tile_size)
 
-    return type(
-        'BruteForceTileMatcher',
-        (object,),
-        {
-            'exclusions': exclusions,
-            'stock_data': stock_data,
-            'find_tile_match': functools.partial(
-                find_tile_match,
-                stock_data=stock_data,
-                exclusions=exclusions,
-            ),
-        },
-    )()
+    return functools.partial(
+        find_tile_match,
+        stock_data=STOCK_DATA,
+        exclusions=exclusions,
+    )
+
+
+# Value is derived from mean + 2 standard deviations which should be within
+# range for 95% of matches
+GOOD_ENOUGH_THRESHOLD = 47396
+
+
+def BruteForceGoodEnoughTileMatcher(tile_size, exclusions=None):
+    if exclusions is None:
+        exclusions = set()
+
+    return functools.partial(
+        find_tile_match,
+        stock_data=STOCK_DATA,
+        exclusions=exclusions,
+        match_threshold=GOOD_ENOUGH_THRESHOLD,
+    )
--- mozy/apps/mosaic/matcher.py
+++ mozy/apps/mosaic/matcher.py
@@ -1,11 +1,10 @@
-import functools
+import time
 
+from django.db import transaction
 from django.conf import settings
 
+from mozy.apps.mosaic.models import NormalizedSourceImage
 from mozy.apps.mosaic.backends import get_mosaic_backend
-from mozy.apps.mosaic.models import SourceImageTile
-
-from multiprocessing import Pool
 
 
 def create_mosaic(mosaic_image, compose_tile_size=None):
@@ -20,7 +19,7 @@ def create_mosaic(mosaic_image, compose_tile_size=None):
 
     for tile in mosaic_image.all_tiles.filter(stock_tile_match__isnull=True):
         tile_data = tile.scipy_tile_data
-        stock_id, match_similarity = matcher.find_tile_match(tile_data)
+        stock_id, match_similarity = matcher(tile_data)
 
         tile.stock_tile_match_id = stock_id
         tile.stock_tile_match_difference = match_similarity
@@ -32,39 +31,16 @@ def create_mosaic(mosaic_image, compose_tile_size=None):
     return mosaic_image
 
 
-def _callback(tile_id, result):
-    stock_id, match_similarity = result
-
-    tile = SourceImageTile.objects.get(pk=tile_id)
-    tile.stock_tile_match_id = stock_id
-    tile.stock_tile_match_difference = match_similarity
-    print "Matched tile:{0} with stock_image:{1} - {2}".format(
-        tile.pk, stock_id, match_similarity,
-    )
-    tile.save()
-
-
-def create_mosaic_async(mosaic_image):
-    """
-    Why is this slower?
-    """
-    already_used_ids = set(mosaic_image.all_tiles.values_list('stock_tile_match', flat=True))
-
-    pool = Pool(processes=4)
-
-    matcher = get_mosaic_backend(settings.MOSAIC_BACKEND)(exclusions=already_used_ids)
-
-    for tile in mosaic_image.all_tiles.filter(stock_tile_match__isnull=True):
-        tile_data = tile.scipy_tile_data
-
-        pool.apply_async(
-            matcher.find_tile_match,
-            (tile_data,),
-            callback=functools.partial(_callback, tile.pk),
-        )
-
-    pool.close()
-    pool.join()
-
-    mosaic_image.compose_mosaic()
-    return mosaic_image
+def worker():
+    while True:
+        pending_work = NormalizedSourceImage.objects.filter(
+            all_tiles__stock_tile_match__isnull=True,
+        ).first()
+        if pending_work:
+            source_image_pk = pending_work.pk
+            with transaction.atomic():
+                source_image = NormalizedSourceImage.objects.select_for_update().get(
+                    pk=source_image_pk,
+                )
+                create_mosaic(source_image)
+        time.sleep(2)
